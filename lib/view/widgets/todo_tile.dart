2175672074acfcca9aee1177d5d0f6a29af8aafe
Anand-Jangid <jangidme88@gmail.com>
2023-07-31 13:13:28 +0530

    Dismissing action added

--- lib/view/widgets/todo_tile.dart
+++ lib/view/widgets/todo_tile.dart
@@ -13,28 +13,39 @@ class TodoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(4.0),
-      child: Card(
-        child: ListTile(
-          leading: const Icon(Icons.task),
-          title: Text(todo.title),
-          subtitle: Text(todo.description),
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => TodoDetailScreen(
-                      todo: todo,
-                    )));
-          },
-          trailing: IconButton(
-            icon: (todo.isCompleted) 
-            ? const Icon(Icons.check_box)
-            : const Icon(Icons.check_box_outline_blank),
-            onPressed: () {
-              Provider.of<TodoProvider>(context, listen: false).updateTodo(Todo(
-                  title: todo.title,
-                  description: todo.description,
-                  isCompleted: !todo.isCompleted,
-                  timeStamp: todo.timeStamp));
+      child: Dismissible(
+        key: Key(todo.title),
+        direction: DismissDirection.horizontal,
+        onDismissed: (direction) {
+          Provider.of<TodoProvider>(context, listen: false).deleteTodo(todo);
+          ScaffoldMessenger.of(context).removeCurrentSnackBar();
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("Todo removed")));
+        },
+        child: Card(
+          child: ListTile(
+            leading: const Icon(Icons.task),
+            title: Text(todo.title),
+            subtitle: Text(todo.description),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => TodoDetailScreen(
+                        todo: todo,
+                      )));
             },
+            trailing: IconButton(
+              icon: (todo.isCompleted)
+                  ? const Icon(Icons.check_box)
+                  : const Icon(Icons.check_box_outline_blank),
+              onPressed: () {
+                Provider.of<TodoProvider>(context, listen: false).updateTodo(
+                    Todo(
+                        title: todo.title,
+                        description: todo.description,
+                        isCompleted: !todo.isCompleted,
+                        timeStamp: todo.timeStamp));
+              },
+            ),
           ),
         ),
       ),
